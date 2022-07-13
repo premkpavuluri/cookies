968331d8d40745f274cafbb3f2f7c28a0ae1b5a3
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-07-13 17:21:03 +0530

    Refactored loginHandler.

--- src/handlers/loginHandler.js
+++ src/handlers/loginHandler.js
@@ -8,26 +8,35 @@ const isUserValid = (username, usersDb) => {
   return usersDb[username];
 };
 
+const handleLoginUser = (req, res, sessions) => {
+  const { username } = req.bodyParams;
+  const session = createSession(username);
+  const { sessionId } = session
+
+  sessions[sessionId] = session;
+
+  res.statusCode = 302;
+  res.setHeader('Set-Cookie', `sessionId=${sessionId}`);
+  res.setHeader('Location', '/guestbook');
+  res.end(`Welcome ${username}`);
+  return;
+};
+
 const loginHandler = (sessions, usersDb) => (req, res, next) => {
   const { pathname } = req.url;
 
   if (pathname !== '/login') {
-    next();
-    return;
+    return next();
   }
 
   const { username } = req.bodyParams;
   if (req.method === 'POST' && isUserValid(username, usersDb)) {
-    const session = createSession(username);
-    const { sessionId } = session
-    sessions[sessionId] = session;
-
-    res.statusCode = 302;
-    res.setHeader('Set-Cookie', `sessionId=${sessionId}`);
-    res.setHeader('Location', '/guestbook');
-    res.end(`Welcome ${username}`);
-    return;
+    return handleLoginUser(req, res, sessions);
   }
+
+  res.statusCode = 302;
+  res.setHeader('Location', '/')
+  res.end('Home page');
 };
 
 module.exports = { loginHandler };
