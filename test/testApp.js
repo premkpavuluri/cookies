6934cefede12f2563d11a8fcae6c81862f7cac1a
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-07-13 10:16:49 +0530

    Added first test to app.

--- test/testApp.js
+++ test/testApp.js
@@ -1,1 +1,17 @@
-const { app } = require('../src/app.js');
+const request = require('supertest');
+const { connectHandlers } = require('../src/app.js');
+
+const appConfig = {
+  resource: './public',
+  db: './db/comments.json'
+};
+
+const app = connectHandlers(appConfig);
+
+describe('Test App', () => {
+  it('Should give 200 on GET /loginpage', (done) => {
+    request(app)
+      .get('/loginpage')
+      .expect(200, done)
+  });
+});
